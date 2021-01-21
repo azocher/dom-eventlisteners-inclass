0e79f47323c98fae7738a1cc0fc23b5cf7b86a37
Anna Zocher <anna.zocher@gmail.com>
2021-01-21 10:46:05 -0800

    added in commented out examples of all three event listener add types

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     
+    // #1: Add event listener with AddEventListener()
     // add a click event listener for button
     document.querySelector("button").addEventListener("click", () => {
         // update Div inner text to Bonjour!
         document.querySelector("div").innerText = "Bonjour!"
     })
-    
+        
 })
 
