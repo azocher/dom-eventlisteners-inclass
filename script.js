15368457a6b3ecb0d0ceacd02ee577ee33ba7d05
Anna Zocher <anna.zocher@gmail.com>
2021-01-21 12:02:55 -0800

    playing around with HTML forms, default behavior, dom events

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Page has loaded!")
     
-    // #1: Add event listener with AddEventListener()
-    // add a click event listener for button
-    document.querySelector("button").addEventListener("click", () => {
-        // update Div inner text to Bonjour!
-        document.querySelector("div").innerText = "Bonjour!"
-    })
+    document.querySelector("form").addEventListener("submit", (event) => {
+        event.preventDefault()
         
+        // get user inputed value from text input
+        let textInput = document.querySelector("input[type='text']").value
+        console.log(textInput)
+        // get user inputed value from color field
+        let colorInput = document.querySelector("input[type='color']").value
+        console.log(colorInput)
+        // get user inputed value from password
+        let passwordInput = document.querySelector("input[type='password']").value
+        console.log(passwordInput)
+
+        // send user input results to inner text of div id="answer"
+        document.getElementById("answer").innerHTML = textInput + colorInput + passwordInput
+    })
 })
 
